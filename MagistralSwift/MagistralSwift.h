06677880939e60a8f04f0a84730a083cf4a4ec8b
roman.kurpatov <roman.kurpatov@magistral.io>
2016-08-02 18:44:14 +0200

    Initial Commit

---
+++ MagistralSwift/MagistralSwift.h
@@ -0,0 +1,19 @@
+//
+//  MagistralSwift.h
+//  MagistralSwift
+//
+//  Created by rizarse on 02/08/16.
+//  Copyright © 2016 magistral.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MagistralSwift.
+FOUNDATION_EXPORT double MagistralSwiftVersionNumber;
+
+//! Project version string for MagistralSwift.
+FOUNDATION_EXPORT const unsigned char MagistralSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagistralSwift/PublicHeader.h>
+
+
